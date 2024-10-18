98d3f06b06239de53a1d961348afca76a5df8465
Vlect <vlect23@gmail.com>
2024-10-18 10:07:28 -0500

    fix: add imports to vit.config file

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import restart from 'vite-plugin-restart'
+import glsl from 'vite-plugin-glsl'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default {
     root: 'src/',
     publicDir: '../static/',
